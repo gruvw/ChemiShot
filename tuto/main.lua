3537961ddb8303ec4d769ab32813d627c7c9dbc7
gruvw <gruvw.dev@gmail.com>
2024-04-16 14:45:55 +0200

    feat(tuto): restablish

--- tuto/main.lua
+++ tuto/main.lua
@@ -1,76 +1,14 @@
 import "CoreLibs/graphics"
-import "CoreLibs/timer"
 
 local gfx <const> = playdate.graphics
-local geo <const> = playdate.geometry
 
+local posX = 0
+local posY = 0
 
-local scene1 = gfx.image.new("scene1")
-local scene2 = gfx.image.new("scene2")
-local scene3 = gfx.image.new("scene3")
-local startScreen = gfx.image.new("start_screen")
-local level_one = gfx.image.new("level_one")
-local currentScene = scene1 -- Start with scene 1
-
-local displayWidth, displayHeight = playdate.display.getSize()
-local overlay = gfx.image.new(displayWidth, displayHeight, gfx.kColorBlack)
-local mask = gfx.image.new(displayWidth, displayHeight)
-
-local transitionDuration = 3000
-local transitionPoint = geo.point.new(260, 90)
-
-local function drawCircleCutout(point, radius)
-    gfx.pushContext(mask)
-        gfx.clear(gfx.kColorWhite)
-        gfx.setColor(gfx.kColorBlack)
-        gfx.fillCircleAtPoint(point, radius)
-    gfx.popContext()
-
-    overlay:setMaskImage(mask)
-    overlay:draw(0, 0)
-end
-
-local function getDistanceToFarthestCorner(point)
-    return math.max(
-        point:distanceToPoint(geo.point.new(0, 0)),
-        point:distanceToPoint(geo.point.new(displayWidth, 0)),
-        point:distanceToPoint(geo.point.new(0, displayHeight)),
-        point:distanceToPoint(geo.point.new(displayWidth, displayHeight))
-    )
-end
-
-local function switchScene()
-    if currentScene == scene1 then
-        currentScene = scene2
-    elseif currentScene == scene2 then
-        currentScene = scene3
-    elseif currentScene == scene3 then
-        currentScene = startScreen
-    else
-        currentScene = scene1 -- Loop back to scene 1 after the start screen
-    end
-end
-
+---@diagnostic disable-next-line: duplicate-set-field
 function playdate.update()
-    currentScene:draw(0, 0)
-    playdate.timer.updateTimers()
-end
-
-function playdate.rightButtonDown()
-    local maxRadius = getDistanceToFarthestCorner(transitionPoint)
-    local timer = playdate.timer.new(transitionDuration, -maxRadius, maxRadius, playdate.easingFunctions.inOutQuad)
-
-    timer.updateCallback = function()
-        local radius = math.abs(timer.value)
-        drawCircleCutout(transitionPoint, radius)
-    end
-
-    -- switch scene at transition midpoint when screen is fully black
-    playdate.timer.performAfterDelay(transitionDuration / 2, switchScene)
-end
-
-function playdate.buttonAButtonDown()
-    if currentScene == startScreen then
-        currentScene = level_one
-    end
+  gfx.clear()
+  posX = posX + 1
+  posY = posY + 1
+  gfx.fillRect(posX, posY, 10, 10)
 end
